16c84c05ad2c0b703ea566de9b2c53e6b0b91b41
Stanislav Smirnov <stanislav@laont.me>
2022-04-24 23:38:30 +0300

    Remove dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,17 +19,6 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:30.1.1-jre")
-}
-
 application {
     // Define the main class for the application.
     mainClass.set("me.laont.chatty.AppKt")
